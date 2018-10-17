d69cfa05af5e78be006f0702e98ba17db4e7047f
Harald Lapp <h.lapp@clipdealer.de>
2018-10-17 17:41:22 +0200

    updated build script to use mkinst

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
-tar cJf - etc usr | cat <(tail -n+$(awk '/^__INSTALLER__/ { print NR + 1; exit 0; }' "${0}") "${0}") - > dist/install.sh
-chmod a+x dist/install.sh
-exit 0
 
-__INSTALLER__
-tail -n+$(awk '/^__ARCHIVE__/ { print NR + 1; exit 0; }' "${0}") "${0}" | tar xopPJv -C /
+#
+# Script to create installation package.
+# Requires mkinst from https://github.com/aurora/mkinst/
+#
+# copyright (c) 2018 by Harald Lapp <harald@octris.org>
+#
 
-echo "done."
-exit 0
+rm -f dist/caddysh_installer.bin
 
-__ARCHIVE__
+mkinst -i <(cat << INSTALLER
+if [ "\$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit 1
+fi 
+
+set -e
+
+tar xzmopP -C / -f \$tmp
+
+mkdir -p /etc/caddy.sh/hosts
+mkdir -p /etc/caddy.sh/templates
+
+for i in php-fpm-global.conf templates/caddy.conf templates/php-fpm-pool.conf; do
+    cp /usr/local/etc/caddy.sh/\$i /etc/caddy.sh/
+done
+INSTALLER
+) dist/caddysh_installer.bin usr
